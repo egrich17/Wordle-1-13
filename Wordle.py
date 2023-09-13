eafd9baf199e8291e48b92795b37e8f8c585a7cf
wb385334 <brooke@woodlandfamily.org>
2023-09-12 23:52:46 -0600

    start to milestone 2

--- Wordle.py
+++ Wordle.py
@@ -13,14 +13,29 @@ from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 
 def wordle():
 
-    def enter_action(s):
-        gw.show_message("You have to implement this method.")
-
+    gw = WordleGWindow()
     N_COLS = 5
     N_ROWS = 6
     guesses_left = 6
 
-    gw = WordleGWindow()
+    def enter_action(s):
+
+        j=0
+        guess_to_check=''
+        while j< N_COLS:
+            # Get the letter in the corresponding column
+            current_letter = gw.get_square_letter(N_ROWS-(guesses_left+1), j)
+            guess_to_check =guess_to_check+current_letter
+            j+=1
+
+        # evaluate if the guess is a word
+        if guess_to_check in FIVE_LETTER_WORDS:
+            gw.show_message("great guess")
+        else:
+            gw.show_message("not in word list")
+        
+
+ 
     gw.add_enter_listener(enter_action)
 
     random_word = random.choice(FIVE_LETTER_WORDS).upper()
@@ -31,6 +46,9 @@ def wordle():
             # Set the letter in the corresponding column
             gw.set_square_letter(N_ROWS - guesses_left, i, letter)
     guesses_left-=1
+
+   
+    
 
 # Startup code
 
