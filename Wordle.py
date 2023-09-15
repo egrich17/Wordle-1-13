170a019e1046132e6663b9e67fc5555bf79b2914
wb385334 <brooke@woodlandfamily.org>
2023-09-14 18:33:28 -0600

    yellow works

--- Wordle.py
+++ Wordle.py
@@ -9,7 +9,7 @@ the 5-letter word of the day.
 import random
 
 from WordleDictionary import FIVE_LETTER_WORDS
-from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
+from WordleGraphics import WordleGWindow, N_COLS, N_ROWS, PRESENT_COLOR, CORRECT_COLOR, MISSING_COLOR
 
 def wordle():
 
@@ -18,15 +18,27 @@ def wordle():
     N_ROWS = 6
     guesses_left = 6
 
+    random_word = random.choice(FIVE_LETTER_WORDS).upper()
+
     def enter_action(s):
 
         j=0
         guess_to_check=''
         while j< N_COLS:
             # Get the letter in the corresponding column
-            current_letter = gw.get_square_letter(N_ROWS-(guesses_left+1), j)
+            current_letter = gw.get_square_letter(gw.get_current_row(), j)
+            print(current_letter)
+            if current_letter in random_word:
+                gw.set_square_color(gw.get_current_row(), j, PRESENT_COLOR)
+            else:
+                gw.set_square_color(gw.get_current_row(), j, MISSING_COLOR)
+
             guess_to_check =guess_to_check+current_letter
             j+=1
+
+        current_row = gw.get_current_row()
+        gw.set_current_row(current_row+1)
+    
 
         # evaluate if the guess is a word
         if guess_to_check.lower() in FIVE_LETTER_WORDS:
@@ -34,11 +46,8 @@ def wordle():
         else:
             gw.show_message("Not in word list. Please try again.")
 
- 
-    gw.add_enter_listener(enter_action)
 
-    #random_word = random.choice(FIVE_LETTER_WORDS).upper()
-    #print(random_word)
+    gw.add_enter_listener(enter_action)
     
     #for i, letter in enumerate(random_word):
         #if i < N_COLS:
