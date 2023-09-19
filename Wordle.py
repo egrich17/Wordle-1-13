5435a36d3aaa8a72d378b359eed10fe8f39516ad
wb385334 <brooke@woodlandfamily.org>
2023-09-18 19:16:06 -0600

    statistics

--- Wordle.py
+++ Wordle.py
@@ -11,12 +11,14 @@ import random
 from WordleDictionary import FIVE_LETTER_WORDS
 from WordleGraphics import WordleGWindow, N_COLS, N_ROWS, PRESENT_COLOR, CORRECT_COLOR, MISSING_COLOR
 
+
+
 def wordle():
 
     gw = WordleGWindow()
     N_COLS = 5
     N_ROWS = 6
-    guesses_left = 6
+    guesses_left = 5
 
     random_word = random.choice(FIVE_LETTER_WORDS).upper()
     print(random_word)
@@ -24,39 +26,70 @@ def wordle():
     def enter_action(s):
 
         j=0
+        i=0
         guess_to_check=''
         while j< N_COLS:
             # Get the letter in the corresponding column
             current_letter = gw.get_square_letter(gw.get_current_row(), j)
-            print(current_letter)
-            if current_letter == random_word[j]:
-                gw.set_square_color(gw.get_current_row(), j, CORRECT_COLOR)
-            elif current_letter in random_word:
-                if gw.get_square_color(gw.get_current_row(), j) != CORRECT_COLOR:
-                    gw.set_square_color(gw.get_current_row(), j, PRESENT_COLOR)
-            else:
-                gw.set_square_color(gw.get_current_row(), j, MISSING_COLOR)
-
             guess_to_check =guess_to_check+current_letter
             j+=1
-
-        current_row = gw.get_current_row()
-        gw.set_current_row(current_row+1)
     
 
         # evaluate if the guess is a word
         if guess_to_check.lower() in FIVE_LETTER_WORDS:
             gw.show_message("Great guess!")
+            guesses_left=guesses_left-1
+            print(guess_to_check)
+            print(random_word)
+            while i < N_COLS:
+                if current_letter == random_word[i]:
+                    gw.set_square_color(gw.get_current_row(), i, CORRECT_COLOR)
+                elif current_letter in random_word:
+                    if gw.get_square_color(gw.get_current_row(), i) != CORRECT_COLOR:
+                        gw.set_square_color(gw.get_current_row(), i, PRESENT_COLOR)
+                else:
+                    gw.set_square_color(gw.get_current_row(), i, MISSING_COLOR)
+                i = i+1
+
+            current_row = gw.get_current_row()
+            gw.set_current_row(current_row+1)
         else:
             gw.show_message("Not in word list. Please try again.")
 
         if guess_to_check.lower() == random_word.lower():
-            gw.show_message("Correct!")
+            end_game()
+
+
 
     gw.add_enter_listener(enter_action)
-    
-    guesses_left-=1
 
+    def end_game():
+
+        one = 0
+        two = 0
+        three = 0
+        four = 0
+        five = 0
+        six = 0
+
+        print(guesses_left)
+
+        if guesses_left == 0:
+            six = six +1
+        elif guesses_left ==1:
+            five = five + 1
+        elif guesses_left ==2:
+            four = four +1
+        elif guesses_left == 3:
+            three = three +1
+        elif guesses_left== 4:
+            two = two +1
+        elif guesses_left== 5:
+            one = one +1
+
+        gw.show_message("Correct! Here are your statistics: \n 1 guess:" + str(one) + "\n 2 guesses:" + str(two) + "\n 3 guesses:" +str(three) + "\n 4 guesses:" + str(four) +"\n 5 guesses:"+ str(five))
+
+    
 # Startup code
 
 if __name__ == "__main__":
